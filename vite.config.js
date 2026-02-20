725a2f6bdab725b3fdf26d6ed223e90e1dff7a4b
iLGentz <gentile.riccardo01@gmail.com>
2026-02-20 16:57:01 +0100

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nome-repo/',  // ← Aggiungi questa linea
+  base: '/it-service-site/',  // ← Aggiungi questa linea
   plugins: [react()],
 })
