a1ce325e51ffa233987eca35b80c19456c521c32
iLGentz <gentile.riccardo01@gmail.com>
2026-02-20 17:28:59 +0100

    fix n_2.2 - last test spero

--- commit.bat
+++ commit.bat
@@ -1,13 +1,7 @@
+@echo off
+
 cd "C:\Users\riccardo.gentile\Desktop\mio\Sito"
 
 set /p msg="Enter commit [n - message]: "
 
-git add .
-
-git commit -m "fix n_%msg%"
-
-git push origin main
-
-npm run deploy
-
-start https://ilgentz.github.io/it-service-site/
+npm run build && git add . && git commit -m "fix n_%msg%" && git push origin main && npm run deploy && start https://ilgentz.github.io/it-service-site/
